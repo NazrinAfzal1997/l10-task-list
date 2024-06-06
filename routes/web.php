9e0e54c815c35c1fa1f8ef205f29a5e8305bf12a
Nazrin Afzal <81605440+NazrinAfzal1997@users.noreply.github.com>
2024-06-07 06:53:26 +0800

    32. Sessions, Errors & Flash Messages

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
         'title' => 'required|string|max:255',
         'description' => 'required|string',
-        'long_description' => 'nullable|string',
+        'long_description' => 'required|string',
     ]);
 
     $task = new Task;
@@ -52,7 +52,7 @@ Route::post('/tasks', function (Request $request) {
     $task->long_description = $data['long_description'];
     $task->save();
 
-    return redirect()->route('tasks.show', ['id' => $task->id]);
+    return redirect()->route('tasks.show', ['id' => $task->id])->with('success', 'Task created successfully');
 })->name('tasks.store');
 
 
